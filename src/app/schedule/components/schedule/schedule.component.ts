869591c4ff3b3cb283be1f95fa1676741cc2dc8b
tomaszzawislak <tomasz.zawislak@j-labs.pl>
2023-06-08 14:47:49 +0200

    ScheduleComponent onPush added

--- src/app/schedule/components/schedule/schedule.component.ts
+++ src/app/schedule/components/schedule/schedule.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ScheduleService} from '../../services/schedule.service';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.scss']
+  styleUrls: ['./schedule.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush // TODO: check on filtering and date change
 })
 export class ScheduleComponent {
   public schedule$ = this.scheduleService.schedule$;
